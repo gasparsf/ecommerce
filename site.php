3627a42c72c9095949fc22d28b2283f6695a1c16
Gaspar Ferreira <gaspar.sf@gmail.com>
2021-04-11 20:46:35 +0100

    Site - Produtos

--- site.php
+++ site.php
@@ -1,11 +1,24 @@
 <?php
 
+use \Hcode\Page;
+use \Hcode\Model\Product;
+use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
+use \Hcode\Model\Address;
+use \Hcode\Model\User;
+use \Hcode\Model\Order;
+use \Hcode\Model\OrderStatus;
+
 $app->get('/', function() {
+
+	$products = Product::listAll();
 
 	$page = new Page();
 
-	$page->setTpl("index");
-	
+	$page->setTpl("index", [
+		'products'=>Product::checkList($products)
+	]);
 });
+	
 
 ?>
