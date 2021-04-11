38fb2bc469bd452698276e7641b15878f7133bb0
Gaspar Ferreira <gaspar.sf@gmail.com>
2021-04-11 23:39:55 +0100

    Detalhe dos produtos

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Claudinha</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Claudinha</a></li><li><a href="/categories/6">Samsung</a></li>
